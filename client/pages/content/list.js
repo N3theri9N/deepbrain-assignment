54506f2d6e9aa47a7f4a3d3b58d9904bcf3c9ccd
N3theri9N <nnea5215@gmail.com>
2022-04-21 16:10:32 +0900

    server - client 간 데이터 호출 테스트
    axios, proxy 대응

--- client/pages/content/list.js
+++ client/pages/content/list.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import axios from "axios";
 
 export default function list () {
+    const proxy = 'http://localhost:4000'
+
+    const loadsacData = () => {
+
+        axios.get(`${proxy}/sac/0`).then((res) => {
+            console.log(res.data); // http://localhost:4000/sac/0 호출 결과를 리턴
+        });
+    }
+
     return <div>
         <div className={"border-bottom mt-4 mb-4"}>
             <h2 className="bd-title" id="content">Content List</h2>
@@ -13,10 +23,12 @@ export default function list () {
             </tr>
             </thead>
             <tbody>
-            <tr>
+            <tr id={"tableRow"}>
                 <td>Data Empty</td>
             </tr>
             </tbody>
         </table>
+        <input type={"button"} onClick={loadsacData} value={"내용 호출 테스트"} />
+
     </div>
 }
--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors')
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
 
 app.get('/', (req, res) => {
     res.send('hello node');
 })
 
-app.get("/sac/:startIndex", (req, res) => {
+app.get("/sac/:startIndex", cors(corsOptions) ,(req, res) => {
     fs.readFile("./contents/csv/SeoulArtCenter2018.csv", "utf-8", (err, data) => {
         let SACList = data.split("\r\n");
         const startIndex = parseInt(req.params.startIndex)
